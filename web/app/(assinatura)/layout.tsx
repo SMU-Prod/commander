5ff093e16711659c9a40cb03f0b2f7f505072916
Russete77 <erickrussomat@gmail.com>
2026-08-07 17:46:10 -0300

    feat: fluxo de assinatura fundador via asaas

---
+++ web/app/(assinatura)/layout.tsx
@@ -0,0 +1,3 @@
+export default function AssinaturaLayout({ children }: { children: React.ReactNode }) {
+  return <div className="mx-auto min-h-dvh w-full max-w-md px-4 pb-10 pt-6">{children}</div>
+}
